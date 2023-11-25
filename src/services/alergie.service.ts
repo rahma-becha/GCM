914abb20607019ce74dd866451664e565fc803a0
sirine-Tr <sirine.trabelsi6@gmail.com>
2023-11-25 17:31:23 +0100

    modification alergie

---
+++ src/services/alergie.service.ts
@@ -0,0 +1,19 @@
+// alergie.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Alergie } from 'src/Model/Alergie';
+ // Assurez-vous d'ajuster le chemin vers votre modèle
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlergieService {
+  private apiUrl = 'http://localhost:3000/alergie'; // Assurez-vous de remplacer cela par l'URL correcte de votre API
+
+  constructor(private http: HttpClient) {}
+
+  getAlergies(): Observable<Alergie[]> {
+    return this.http.get<Alergie[]>(this.apiUrl);
+  }
+}
