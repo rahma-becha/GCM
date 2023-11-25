7cadc51f4375707083e5da480e3b0f8a6c36c42e
rahmabecha2 <becha.rahma.55@gmail.com>
2023-11-25 16:43:10 +0100

    Edit RDV

--- src/app/list-rv/list-rv.component.ts
+++ src/app/list-rv/list-rv.component.ts
@@ -10,7 +10,7 @@ import { RendezvousService } from 'src/services/rendrezvous.service';
 })
 export class ListRVComponent implements OnInit {
   rendezvousList:Rendezvous[]=[]
-  statuts: string[] = ["En attente d'arrivée", "En cours", "Clôture"];
+  statuts: string[] = ["En attente d'arrivée", "En cours", "Clôture","Annulé"];
 
   constructor(private rendezvousService:RendezvousService){
 
@@ -24,16 +24,30 @@ export class ListRVComponent implements OnInit {
       })
     }
 
-    deleteRDV(id: number | undefined) {
-      if (confirm("Vous êtes sure de supprimer cette patient")) {
-        if (id != undefined) {
-          this.rendezvousService.deleteRendezvous(id).subscribe(data => {
+    deleteRDV(rdv:Rendezvous) {
+      if (confirm("Vous êtes sure d'annulé cette patient")) {
+          rdv.status="Annulé"
+          this.rendezvousService.updateRendezvous(rdv).subscribe(data => {
             this.getAllRDVs()
           }
           )
-        }
+        
   
       }
   
     }
+    setColutre(rdv:Rendezvous){
+      rdv.status="Clôture"
+      this.rendezvousService.updateRendezvous(rdv).subscribe(data => {
+        this.getAllRDVs()
+      }
+      )
+    }
+  setEnCours(rdv:Rendezvous){
+    rdv.status="En cours"
+          this.rendezvousService.updateRendezvous(rdv).subscribe(data => {
+            this.getAllRDVs()
+          }
+          )
+  }
 }
