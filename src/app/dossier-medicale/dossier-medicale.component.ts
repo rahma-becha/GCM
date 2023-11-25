9abc197df7b1531e190863ff46bbd45615b742b5
sirine-Tr <sirine.trabelsi6@gmail.com>
2023-11-25 16:16:16 +0100

    tout les formulaire de DM

--- src/app/dossier-medicale/dossier-medicale.component.ts
+++ src/app/dossier-medicale/dossier-medicale.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dossier-medicale.component.css']
 })
 export class DossierMedicaleComponent {
+  selectedFile: File | undefined;
 
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 }
